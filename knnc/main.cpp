9cd51ce1c6b9e0c003c714f3e59b1d2631f02fea
abhishek singh <abhishek.s14@iiits.in>
2016-04-15 21:09:23 +0530

    removes bug

--- knnc/main.cpp
+++ knnc/main.cpp
@@ -2,6 +2,8 @@
 #include<cstdio>
 #include<cstdlib>
 #include<algorithm>
+#include<bits/stdc++.h>
+#include <sys/resource.h>
 
 
 #define TRAININGFILENAME "pp_tra.dat"
@@ -13,6 +15,8 @@
 #define TEST_DATA 3333
 
 using namespace std;
+
+int input[TRAIN_DATA][FEATURES+1];
 
 struct Dist{
   long int distance;
@@ -90,12 +94,52 @@ void reset( int freq[] , int &m , Dist distance[] ){
 }
 
 int main(){
+
+
+
+
+
+
+
+
+
+
+        const rlim_t kStackSize = 24 * 1024 * 1024;   // min stack size = 16 MB
+        struct rlimit rl;
+        int result;
+
+        result = getrlimit(RLIMIT_STACK, &rl);
+        if (result == 0)
+        {
+                if (rl.rlim_cur < kStackSize)
+                {
+                        rl.rlim_cur = kStackSize;
+                        result = setrlimit(RLIMIT_STACK, &rl);
+                        if (result != 0)
+                        {
+                                fprintf(stderr, "setrlimit returned result = %d\n", result);
+                        }
+                }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
   int x;
-  cout<<"Flag 1"<<endl;
+  cout<<"Started program"<<endl;
 
 //  int input[TRAIN_DATA][FEATURES+1];
 //  readData( TRAININGFILENAME , input );
-  int test_input[TEST_DATA][FEATURES+1],a,error[TRAIN_DATA][M+1],input[TRAIN_DATA][FEATURES+1],min_index,result_class,original_class,partition_size = TRAIN_DATA/FOLDS,freq[10],m,pos=-1,final_k;
+  int test_input[TEST_DATA][FEATURES+1],a,error[TRAIN_DATA][M+1],min_index,result_class,original_class,partition_size = TRAIN_DATA/FOLDS,freq[10],m,pos=-1,final_k,test_error;
   float finalError[FOLDS][M+1],min=1000.0;
 
   Dist distance[TRAIN_DATA];
@@ -103,7 +147,7 @@ int main(){
 
   readData( TRAININGFILENAME , input );
   randomizeData( input , TRAIN_DATA );
- // readData( TESTINGFILENAME , test_input );
+//  readData( TESTINGFILENAME , test_input );
 
 /*
   for( int i=0; i<TEST_DATA; i++ ){
@@ -115,9 +159,12 @@ int main(){
   }
  // cin>>x;
 */
+
   for( int i=0; i<FOLDS; i++ ){
+//    cout<<"!!"<<endl;
 //    cout<<"Starting of cross validation is "<<i*partition_size<<" and ending of cross validation set is "<<(1+i)*partition_size<<endl;
     for( int j=i*partition_size; j<(1+i)*partition_size; j++ ){
+//      cout<<"--"<<endl;
       for( int l=0; l<TRAIN_DATA; l++ ){
         if( !( l>=i*partition_size && l<(1+i)*partition_size) ){
           distance[m].index=l;
@@ -142,8 +189,8 @@ int main(){
     }
   }
 //  cout<<"Flag1"<<endl;
-/*  
-    for( int i=0; i<FOLDS; i++ ){
+  
+  for( int i=0; i<FOLDS; i++ ){
     //int sum=0;
     for( int k=1; k<=M; k++ ){
       int sum=0;
@@ -173,6 +220,9 @@ int main(){
   final_k = pos;
   cout<<"Optimal k is "<<final_k<<endl;
 
+  test_error = 0;
+
+  readData( TESTINGFILENAME , test_input );
 
   for( int j=0; j<TEST_DATA; j++ ){
     for( int l=0; l<TRAIN_DATA; l++ ){
@@ -184,12 +234,16 @@ int main(){
         freq[input[distance[k].index][FEATURES]]++;
     result_class = std::distance( freq , max_element( freq , freq + 10 ) );
     original_class = test_input[j][FEATURES];
-    if( result_class != original_class )
-      cout<<"Result class is "<<result_class<<" and original class is "<<original_class<<endl;
+    if( result_class != original_class ){
+ //     cout<<"Result class is "<<result_class<<" and original class is "<<original_class<<endl;
+      test_error++ ;
+    }
     else
-      cout<<"Pass"<<endl;
+ //     cout<<"Pass"<<endl;
     reset(freq,m,distance);
   }
-*/
+
+  cout<<"Error is "<<((float)test_error/TEST_DATA)*100.0<<endl;
+
   return 0;
 }
